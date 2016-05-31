56a7f41b962e3bfb65a99790a7a953d93bf4cc4a
unknown <karolis.gric@gmail.com>
2016-05-31 16:13:40 +0300

    added redirect

--- index.js
+++ index.js
@@ -169,6 +169,10 @@ app.get('/removeReq', function(req, res) {
 	res.json({});
 });
 
+app.get('/*', function(request, response) {
+		response.redirect('/');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
